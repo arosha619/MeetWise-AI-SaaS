ef1992a3c8f9fab5a081c085e000eaf1161705ce
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2026-01-10 17:29:24 +0530

    protect route , flash error fixed and sign out

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { data: session, isPending } = authClient.useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isPending && !session) {
+      router.push("/auth/sign-in");
+    }
+  }, [isPending, session, router]);
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+
+  if (!session) {
+    return null;
+  }
   return (
-<Calendar></Calendar>
+    <>
+      Home page
+      <p>Logged in as :{session?.user.name}</p>
+      <div className="">
+        <button
+          onClick={async () => {
+            try {
+              authClient.signOut();
+            } catch (err) {
+              console.log("Error signing out:", err);
+            } finally {
+              router.push("/auth/sign-in");
+            }
+          }}
+        >
+          {" "}
+          sign Out
+        </button>
+      </div>
+    </>
   );
 }
