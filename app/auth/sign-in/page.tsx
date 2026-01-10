8b782afe3e466c941e06fdf1e9bcf52324113470
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2026-01-10 18:47:49 +0530

    protects routes

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,6 +1,16 @@
 import SigninView from "@/app/modules/auth/ui/views/signin-view"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+  });
+  
+    if (!!session) {
+      redirect("/");
+    }
   return (
     <main className="min-h-screen flex items-center justify-center bg-muted p-4">
       <SigninView />
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,9 +1,19 @@
-import SignupView from "@/app/modules/auth/ui/views/signup-view"
+import SignupView from "@/app/modules/auth/ui/views/signup-view";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
   return (
     <main className="min-h-screen flex items-center justify-center bg-muted p-4">
       <SignupView />
     </main>
-  )
+  );
 }
