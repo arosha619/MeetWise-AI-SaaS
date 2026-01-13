f66f80adfcfa614a6e517cf1b8923afdb4a095c7
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2026-01-13 22:38:09 +0530

    dashboard command

--- app/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ app/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -1,13 +1,18 @@
 "use client";
-
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Kbd } from "@/components/ui/kbd";
 import { useSidebar } from "@/components/ui/sidebar";
 import { PanelLeftCloseIcon, PanelLeftIcon, SearchIcon } from "lucide-react";
+import { DashboardCommand } from "./dashboard-command";
+
 
 export const DashboardNavBar = () => {
   const { state, toggleSidebar, isMobile } = useSidebar();
+  const [commandOpen, setCommandOpen] = useState(false);
   return (
+    <>
+    <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
     <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
       <Button className="size-9" variant="outline" onClick={toggleSidebar}>
         {state === "collapsed" || isMobile ? (
@@ -20,7 +25,7 @@ export const DashboardNavBar = () => {
         className="h-9 w-66.5 justify-start font-normal text-muted-foreground hover:text-muted-foreground"
         variant="outline"
         size="sm"
-        onClick={() => {}}
+        onClick={() => setCommandOpen((open)=>!open)}
       >
         <SearchIcon />
         Search agents, Meetings and all...
@@ -29,5 +34,6 @@ export const DashboardNavBar = () => {
         </Kbd>
       </Button>
     </nav>
+    </>
   );
 };
